c928e80fa4b4c23b4133a83f45e2f283408a541a
AlanDReeves <alandanielreeves@gmail.com>
2025-08-27 09:52:06 -1000

    Update twoSum.cpp
    Finished and working, but slow.

--- twoSum.cpp
+++ twoSum.cpp
@@ -2,17 +2,19 @@
 # include<vector>
 
 class Solution {
-    // Doesn't work yet. Currently always reaches return garbage
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
         std::vector<std::pair<int, int>> seen = std::vector<std::pair<int, int>>();
         for (int i = 0; i < int(nums.size()); i++) {
+            seen.emplace_back(std::pair<int, int>({nums[i], i}));
+            if (seen.size() == 1) {
+                continue;
+            }
             for (int j = 0; j < int(seen.size()); j++) {
-                if (nums[i] - seen[j].first == target) {
+                if (nums[i] + seen[j].first == target && seen[j].second != i) {
                     std::vector<int> ans = {i, seen[j].second};
                     return ans;
                 }
-                seen.emplace_back(std::pair<int, int>({nums[i], i}));
             }
         }
         std::vector<int> garbage = {-1, -1}; // return garbage answer if no real answer found
