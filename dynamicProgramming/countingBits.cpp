98a9e99fc855c288b1481cbeb03d4f32ba2be310
AlanDReeves <alandanielreeves@gmail.com>
2025-10-21 16:03:20 -1000

    Create countingBits.cpp

---
+++ dynamicProgramming/countingBits.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans = vector<int>();
+        vector<int> memos(n + 1, -1);
+        for (int i = 0; i <= n; i++) {
+            ans.emplace_back(countForGiven(i, memos));
+        }
+        return ans;
+    }
+
+    int countForGiven(int n, vector<int>& memos) {
+        if (memos[n] != -1) return memos[n];
+
+        int ones = 0;
+        while (n > 0) {
+            if (n % 2) ones++;
+            n /= 2;
+        }
+        memos[n] = ones;
+        return ones;
+    }
+};
